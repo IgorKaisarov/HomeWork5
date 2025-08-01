fdd16fd0cdd679fc571da1809ef1d09861b70ab9
IgorKaisarov <Igo444@mail.ru>
2025-08-01 14:57:11 +0600

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Home Work 5");
+        System.out.printf(" Task 1 ");
 
-        int clientOS = 0;
+        int clientOS = 1;
         switch (clientOS) {
             case 0:
                 System.out.println(" Установите версию приложения для iOS по ссылке ");
@@ -15,15 +15,22 @@ public class Main {
                 System.out.println(" Операционная система не найдена ");
         }
 
+        System.out.printf(" Task 2 ");
 
-
-
-
-
-
-
-
-
+        int clientOS1 = 0;
+        int clientDeviceYear = 2017;
+        if (clientOS1 == 1) ;
+        if (clientDeviceYear < 2015) {
+            System.out.println(" Установите облегченную версию Android");
+        } else {
+            System.out.println(" Установите версию приложения для Android по ссылке");
+            if (clientOS1 == 0) ;
+            if (clientDeviceYear < 2015) {
+                System.out.println(" Установите облегченную версию iOS");
+            } else {
+                System.out.println(" Установите версию приложения для iOS по ссылке");
+            }
+        }
 
 
         for (int i = 1; i <= 5; i++) {
